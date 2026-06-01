954d16e7e427a515350bda4e5bfb48267ba89e48
Eya <e.elibrahimi@sentinum.de>
2026-06-01 15:33:54 +0200

    ifm sensors html cddes

---
+++ fix_links.ps1
@@ -0,0 +1,8 @@
+﻿$count = 0
+$files = Get-ChildItem ".\Ifm documentation" -Recurse -Filter "*.html"
+foreach ($file in $files) {
+    $content = [System.IO.File]::ReadAllText($file.FullName)
+    $new = $content -replace 'https://github\.com/bellaminemo81172/sentinum_documentation/blob/', 'https://raw.githubusercontent.com/bellaminemo81172/sentinum_documentation/'
+    if ($new -ne $content) { [System.IO.File]::WriteAllText($file.FullName, $new); $count++ }
+}
+Write-Host "Fixed $count files"
